f0ef2170372556e5e125fcfd86dac74a406f7142
Volodymyr Mazur <mazur.volodymyr@gmail.com>
2023-03-02 22:15:01 +0200

    Step12. Web app. Trying to fix coveralls report not showing v.2

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,8 +3,4 @@ import unittest
 # from .test_intt_views import TestViews
 # from .test_intt_event_api import TestEventAPI
 # from .test_intt_pass_api import TestPassengerAPI
-#from .test_simple_mock import TestSimpleTest
-from .test_DB_ops_mock import TestNewEnv
-
-if __name__ == '__main__':
-    unittest.main()
+from .test_intt_event_api import TestEventAPI
