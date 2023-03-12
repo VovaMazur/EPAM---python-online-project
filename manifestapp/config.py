2f008471d3786edaa67b52212749b4fdbadf5442
Volodymyr Mazur <mazur.volodymyr@gmail.com>
2023-03-12 15:16:20 +0200

    Step12. Web app. Config changed. Final code

--- manifestapp/config.py
+++ manifestapp/config.py
@@ -1,6 +1,6 @@
 """Manifest application config file"""
 import os
 
-SQLALCHEMY_DATABASE_URI = 'mysql://app:password@192.168.0.111:3306/manifestapp'
+SQLALCHEMY_DATABASE_URI = 'mysql://app:password@192.168.0.121:3306/manifestapp'
 SECRET_KEY = os.urandom(12)
 ENV = 'production'
